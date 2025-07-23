0df98314d673f7ea6af3ce701d4dae84aed60908
Valery Fremaux <valery.fremaux@gmail.com>
2025-07-23 11:36:49 +0200

    Committing hook changes

---
+++ classes/hooks/output/before_standard_html_head.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hooks implementation.
+ *
+ * @package    theme_klassplace
+ */
+namespace mod_sharedresource\hook\output;
+
+class before_standard_html_head {
+
+    /**
+     * Fixes an XSS risk on login form by sanitizing the received token.
+     */
+    public static function callback(\core\hook\output\before_standard_html_head $hook): void {
+        $isframetop = optional_param('frameset', '', PARAM_ALPHA);
+        if ($isframetop) {
+            return '<base target="_top" />';
+        }
+    }
+}
